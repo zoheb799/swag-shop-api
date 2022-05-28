d8677af3b9c576bbc2ea8211dff793b6c31c7ed0
zoheb799 <96819670+zoheb799@users.noreply.github.com>
2022-05-28 19:38:07 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,20 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-// var db = mongoose.connect('mongodb://localhost/dev');
+var DB = mongoose.connect('mongodb+srv://zero:<zero>@cluster0.nu0uy.mongodb.net/zero?retryWrites=true&w=majority').then(() => {
+console.log('connection to DB successful');
+}).catch((err) => console.log('no connection to DB'));
+
+
+// // }).catch((err) => console.log('no connection to DB'));
+// // ;
+// // connect to DB
+// // const DB = 'mongodb+srv://zero:<zero>@cluster0.nu0uy.mongodb.net/zero?retryWrites=true&w=majority';
+// // mongoose.connect(DB).then(() => {
+// console.log('connection to DB successful');
+
+// }).catch((err) => console.log('no connection to DB'));
+
 
 var product = require('./models/product');
 var WishList = require('./models/wishlist');
@@ -10,24 +23,68 @@ var WishList = require('./models/wishlist');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
+var ingredients = [
+    {
+      "id":"983j",
+      "text":"chicken biryani",
+    },
+  
+    {
+      "id":"983j", 
+      "text":"veg biryani",
+    },
+  
+    {
+      "id":"983j",
+      "text":"mutton biryani",
+    },
+    {
+      "id":"983j",
+      "text":"prawns biryani",
+    },
+    {
+      "id":"983j",
+      "text":"water bottle or cool drinks",
+    },
+  ];
+  app.get('/Menu', function(request, response) {
+    response.send(ingredients);
+  }); 
 
-app.post('./product', function(request, response) {
-    var product = new product();
+app.post('/product', function(request, response) {
+    var Product = new product();
     product.title = request.body.title;
     product.price = request.body.price;
-    product.save(function(err, savedproduct) {
-        if(err) {
+    Product.save(function(err, SavedProduct) {
+        if (err) {
             response.status(500).send({error:"could not save the product"});
         } else {
-            response.send(savedproduct);
+            response.send(SavedProduct);
         }
     });
    
 });
-// connect to DB
-mongoose.connect('mongodb+srv://zohaib:<zohaib>@dev.rqpe6.mongodb.net/?retryWrites=true&w=majority', () =>
- console.log('connected to DB!')
- );
+
+// app.post('/product', function(request, response) {
+//   var product = new Product();
+//     if (!product || product.text === "") {
+//       response.status(500).send({error: "enter your details"})
+//     } else {
+//       product.push(product);
+//       response.status(200).send(product);
+//     }
+//    });
+
+
+
+
+
+
+
+
+
+
+
 
 
 app.listen(3000, function() {
